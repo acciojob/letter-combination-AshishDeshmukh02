a148c988521ed0f2b1f5e99ee4bfce2eda95192a
Ashish Deshmukh <115146337+AshishDeshmukh02@users.noreply.github.com>
2023-02-08 13:36:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function letterCombinations(input_digit) {
   //Complete the function
+	if (!digits.length) return [];
+  
+  const mapping = ['0', '1', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz'];
+  let result = [''];
+  
+  for (const digit of digits) {
+    const letters = mapping[digit];
+    const temp = [];
+    for (const res of result) {
+      for (const letter of letters) {
+        temp.push(res + letter);
+      }
+    }
+    result = temp;
+  }
+  
+  return result;
 }
 
 module.exports = letterCombinations;
